834264d62c632b1261e310264ebb2ea220679a24
Kenneth Jiang <kenneth.s.jiang@gmail.com>
2024-02-28 14:30:08 -0500

    Update __init__.py

--- src/whisper_ctranslate2/__init__.py
+++ src/whisper_ctranslate2/__init__.py
@@ -1,9 +1,1 @@
-import os
-import warnings
-import logging
-from ._version import __version__
-
-__all__ = ["__version__"]
-
-warnings.filterwarnings("ignore")
-warnings.simplefilter("ignore")
+# empty version file 
